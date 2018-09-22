74ffe8c9911f8261679e7dea02f8eeef423e8452
Christine Luo <christineluo@berkeley.edu>
2018-09-21 22:20:58 -0700

    finished

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,6 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map { |e| e.to_i+2 }.select{ |num| num.even? }.uniq.select{ |num| num < 10}.reduce(:+)
   end
 end
